f7f966765e73bc3853d54193324f7f23df25fcb7
nrsimonelli <nrsimonelli@gmail.com>
2021-06-23 16:19:36 -0500

    major theme update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Nav from './Nav/Nav';
 
 const App = () => {
   return (
     <div className='root wave-layer'>
+      <Nav />
       <h1>hello friend</h1>
     </div>
   ); // end return
---
+++ src/Nav/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+    <div className='nav-root'>
+      <h1>nav component</h1>
+    </div>
+  ); // end return
+};
+
+export default Nav;
