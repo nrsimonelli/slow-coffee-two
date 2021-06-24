b400d5ad4138db991bcb7b4cd5d7986097c0e1ba
nrsimonelli <nrsimonelli@gmail.com>
2021-06-24 18:00:15 -0500

    more theme work, landing page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Landing from './Landing/Landing';
 
 const App = () => {
   return (
-    <div className='root wave-layer min-h-screen flex justify-start items-center'>
+    <div className='root min-h-screen flex justify-start items-center'>
       <Nav />
       <Landing />
     </div>
