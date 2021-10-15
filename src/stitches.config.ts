32f66df7e930560cdcbc850146c8b967371cd594
nrsimonelli <nrsimonelli@gmail.com>
2021-10-14 21:44:16 -0500

    base utils and darktheme placeholder

--- src/stitches.config.ts
+++ src/stitches.config.ts
@@ -24,7 +24,70 @@ export const { styled, config, theme, createTheme, globalCss } =
       transitions: {},
     },
     media: {},
-    utils: {},
+    utils: {
+      bc: (value: Stitches.PropertyValue<'backgroundColor'>) => ({
+        backgroundColor: value,
+      }),
+      p: (value: Stitches.PropertyValue<'padding'>) => ({
+        padding: value,
+      }),
+      pt: (value: Stitches.PropertyValue<'paddingTop'>) => ({
+        paddingTop: value,
+      }),
+      pr: (value: Stitches.PropertyValue<'paddingRight'>) => ({
+        paddingRight: value,
+      }),
+      pb: (value: Stitches.PropertyValue<'paddingBottom'>) => ({
+        paddingBottom: value,
+      }),
+      pl: (value: Stitches.PropertyValue<'paddingLeft'>) => ({
+        paddingLeft: value,
+      }),
+      px: (value: Stitches.PropertyValue<'paddingLeft'>) => ({
+        paddingLeft: value,
+        paddingRight: value,
+      }),
+      py: (value: Stitches.PropertyValue<'paddingTop'>) => ({
+        paddingTop: value,
+        paddingBottom: value,
+      }),
+      m: (value: Stitches.PropertyValue<'margin'>) => ({
+        margin: value,
+      }),
+      mt: (value: Stitches.PropertyValue<'marginTop'>) => ({
+        marginTop: value,
+      }),
+      mr: (value: Stitches.PropertyValue<'marginRight'>) => ({
+        marginRight: value,
+      }),
+      mb: (value: Stitches.PropertyValue<'marginBottom'>) => ({
+        marginBottom: value,
+      }),
+      ml: (value: Stitches.PropertyValue<'marginLeft'>) => ({
+        marginLeft: value,
+      }),
+      mx: (value: Stitches.PropertyValue<'marginLeft'>) => ({
+        marginLeft: value,
+        marginRight: value,
+      }),
+      my: (value: Stitches.PropertyValue<'marginTop'>) => ({
+        marginTop: value,
+        marginBottom: value,
+      }),
+      clip: (value: Stitches.PropertyValue<'backgroundClip'>) => ({
+        WebkitBackgroundClip: value,
+        backgroundClip: value,
+      }),
+      gradient: (
+        value: Stitches.PropertyValue<'backgroundImage'>
+      ) => ({
+        backgroundImage: `linear-gradient(${value})`,
+      }),
+    },
   });
 
 export type CSS = Stitches.CSS<typeof config>;
+
+export const darkTheme = createTheme('dark-theme', {
+  colors: {},
+});
